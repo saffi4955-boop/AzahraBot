2142c4a41218ff6c8033c0063be0d677ac560fa8
azar tech <mohamedazarudeen011@gmail.com>
2026-04-30 00:02:04 +0530

    Improve comments in settings.js for clarity
    Updated comments for bot owner configuration.

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  botOwner: "Azar",
-  ownerNumber: "917358646943",
-  ownerLid: "92780588216445@lid",
+  botOwner: "Azar", // Replace to your name 
+  ownerNumber: "917358646943", // Replace to your number 
+  ownerLid: "92780588216445@lid", // get your own @lid and paste it here.
   prefix: ".",
   version: "5.2.1",
   commandMode: "public",
